ca2fb4ca8517c1bf9fff454ce3fbe520df074936
rufus peter lobo <rufuslobo@eduroam-2-137.epfl.ch>
2017-11-11 19:28:36 +0100

    Basic controller methods for SkyScanner API invocation and python script execution test

--- LauzHack-Skyscanner/Controllers/HomeController.cs
+++ LauzHack-Skyscanner/Controllers/HomeController.cs
@@ -22,6 +22,16 @@ namespace LauzHack_Skyscanner.Controllers
         }
     }
 
+    public class Destination
+    {
+        //TODO: Add destination properties here
+
+        public Destination() 
+        {
+            //TODO: Constructor to instantiate a destination
+        }
+    }
+
     public class HomeController : Controller
     {
         public IActionResult Index()
@@ -32,9 +42,41 @@ namespace LauzHack_Skyscanner.Controllers
             return View(friendList);
         }
 
-        public IActionResult AddFriend()
+        public List<string> AutoComplete()
         {
-            return View();    
+            //TODO: Read request parameters to get input string for Origin Destination
+            var origin = "";
+
+            List<string> originList = new List<string>();
+            originList.Add("Response1");
+            originList.Add("Response2");
+            originList.Add("Response3");
+
+            //TODO: Method call to SkyScanner here
+
+            return originList;
+        }
+
+        public IActionResult Destinations()
+        {
+            List<Friend> friendList = new List<Friend>();
+            //TODO: Read list of friends from request parameters into List
+            friendList.Add(new Friend());
+            friendList.Add(new Friend());
+            friendList.Add(new Friend());
+
+            List<Destination> destinationList = new List<Destination>();
+            destinationList.Add(new Destination());
+            destinationList.Add(new Destination());
+            destinationList.Add(new Destination());
+
+            return View(new Tuple<IEnumerable<Friend>, IEnumerable<Destination>>(friendList, destinationList));    
+        }
+
+        public IActionResult TestRunPythonScript()
+        {
+            ViewData["Output"] = "Put output from python as a string here";
+            return View();
         }
 
         public IActionResult About()
