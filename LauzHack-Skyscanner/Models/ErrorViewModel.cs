6d5b5883c40ecdca96d1fa3d232bb31f8075b33e
rufus peter lobo <rufuslobo@eduroam-2-137.epfl.ch>
2017-11-11 13:35:54 +0100

    Initial Default WebApp

---
+++ LauzHack-Skyscanner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LauzHack_Skyscanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
