4e0bc5e8cd120ea2adeb2cbe7077c0f4f64f8b7d
rufus peter lobo <rufuslobo@eduroam-2-137.epfl.ch>
2017-11-11 20:59:00 +0100

    Basic addition of friend to friendlist

--- LauzHack-Skyscanner/wwwroot/js/site.js
+++ LauzHack-Skyscanner/wwwroot/js/site.js
@@ -1,10 +1,28 @@
 ﻿$(document).ready(function () {
 
     //TODO: Get destination visibility
+    var friendList = [];
+
     function validateFriendDetails() {
         return true;
     }
 
+    function repopulateFriendListTable() {
+        var friendTableHtml = "";
+
+        for (var i=0; i<friendList.length; i++) {
+            friendTableHtml += "<tr>";
+            friendTableHtml += "<td>" + friendList[i].serialNo + "</td>";
+            friendTableHtml += "<td>" + friendList[i].name + "</td>";
+            friendTableHtml += "<td>" + friendList[i].origin + "</td>";
+            friendTableHtml += "<td>" + friendList[i].isReturn + "</td>";
+            friendTableHtml += "<td>" + friendList[i].departureDate + "</td>";
+            friendTableHtml += "<td>" + friendList[i].returnDate + "</td>";
+            friendTableHtml += "</tr>";
+        }
+
+        $("#friendList > tbody").html(friendTableHtml);
+    }
 
     $("#addFriendFormDiv").hide();
     $("#showFriendForm").show();
@@ -18,10 +36,25 @@
     });
 
     $("#addFriendToList").click(function() {
-        
+        if(validateFriendDetails()) {
+            var serialNo = friendList.length + 1;
+            alert(serialNo);
+            var friend = {
+                serialNo: serialNo, 
+                name: $("#Name").val(), 
+                origin: $("#Origin").val(),
+                isReturn: $("#IsReturn").val(),
+                departureDate: $("#DepartureDate").val(),
+                returnDate: $("#ReturnDate").val()
+            };
 
-        $("#showFriendForm").show();
-        $("#addFriendFormDiv").hide();
+            friendList.push(friend);
+            repopulateFriendListTable();
+
+            $("#showFriendForm").show();
+            $("#addFriendFormDiv").hide();
+
+        }
     });
 
     $("#getDestination").click();
